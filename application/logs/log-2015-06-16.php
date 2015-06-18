ea36ebeb0abf9f47f56f6d1eff6e0d9827c97f96
Titus Tunduny <ttunduny@gmail.com>
2015-06-18 16:25:14 +0300

    CLC

---
+++ application/logs/log-2015-06-16.php
@@ -0,0 +1,35 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2015-06-16 10:07:23 --> Severity: Notice --> Undefined variable: log_status /opt/lampp/htdocs/HCMP/application/controllers/User_management.php 73
+ERROR - 2015-06-16 10:08:15 --> Severity: Notice --> Undefined offset: 0 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 293
+ERROR - 2015-06-16 10:08:15 --> Severity: Notice --> Undefined offset: 1 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 293
+ERROR - 2015-06-16 10:08:15 --> Severity: Notice --> Undefined offset: 2 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 293
+ERROR - 2015-06-16 10:08:15 --> Severity: Notice --> Undefined offset: 3 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 293
+ERROR - 2015-06-16 10:08:15 --> Severity: Notice --> Undefined offset: 4 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 293
+ERROR - 2015-06-16 10:13:37 --> 404 Page Not Found: Home/index
+ERROR - 2015-06-16 10:13:47 --> Severity: Notice --> Undefined variable: highest_stocks_details /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 195
+ERROR - 2015-06-16 10:13:48 --> Severity: Notice --> Undefined offset: 0 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 169
+ERROR - 2015-06-16 10:13:48 --> Severity: Notice --> Undefined offset: 1 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 169
+ERROR - 2015-06-16 10:13:48 --> Severity: Notice --> Undefined offset: 2 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 169
+ERROR - 2015-06-16 10:13:48 --> Severity: Notice --> Undefined offset: 3 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 169
+ERROR - 2015-06-16 10:13:48 --> Severity: Notice --> Undefined offset: 4 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 169
+ERROR - 2015-06-16 10:13:48 --> Severity: Notice --> Undefined variable: highest_stocks_details /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 195
+ERROR - 2015-06-16 10:13:48 --> Severity: Notice --> Undefined variable: highest_stocks_details /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 195
+ERROR - 2015-06-16 10:13:48 --> Severity: Notice --> Undefined variable: highest_expiries_details /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 346
+ERROR - 2015-06-16 10:13:48 --> Severity: Notice --> Undefined variable: highest_expiries_details /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 346
+ERROR - 2015-06-16 10:13:48 --> Severity: Notice --> Undefined variable: highest_expiries_details /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 346
+ERROR - 2015-06-16 10:13:50 --> Severity: Notice --> Undefined offset: 0 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 293
+ERROR - 2015-06-16 10:13:50 --> Severity: Notice --> Undefined offset: 1 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 293
+ERROR - 2015-06-16 10:13:50 --> Severity: Notice --> Undefined offset: 2 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 293
+ERROR - 2015-06-16 10:13:50 --> Severity: Notice --> Undefined offset: 3 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 293
+ERROR - 2015-06-16 10:13:50 --> Severity: Notice --> Undefined offset: 4 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 293
+ERROR - 2015-06-16 10:13:55 --> Severity: Notice --> Undefined offset: 0 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 293
+ERROR - 2015-06-16 10:13:55 --> Severity: Notice --> Undefined offset: 1 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 293
+ERROR - 2015-06-16 10:13:55 --> Severity: Notice --> Undefined offset: 2 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 293
+ERROR - 2015-06-16 10:13:55 --> Severity: Notice --> Undefined offset: 3 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 293
+ERROR - 2015-06-16 10:13:55 --> Severity: Notice --> Undefined offset: 4 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 293
+ERROR - 2015-06-16 10:14:23 --> Severity: Notice --> Undefined offset: 0 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 293
+ERROR - 2015-06-16 10:14:23 --> Severity: Notice --> Undefined offset: 1 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 293
+ERROR - 2015-06-16 10:14:23 --> Severity: Notice --> Undefined offset: 2 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 293
+ERROR - 2015-06-16 10:14:23 --> Severity: Notice --> Undefined offset: 3 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 293
+ERROR - 2015-06-16 10:14:23 --> Severity: Notice --> Undefined offset: 4 /opt/lampp/htdocs/HCMP/application/controllers/Clc_management.php 293
